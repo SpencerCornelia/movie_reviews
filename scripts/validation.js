96a54cb78caf9fde0ee1c877086b5ab9b71879fd
Spencer Cornelia <cornelia.spencer@yahoo.com>
2017-02-18 11:56:43 -0800

    changed name of both requests to allow for asynchronous behavior

--- scripts/validation.js
+++ scripts/validation.js
@@ -9,9 +9,9 @@ function checkUsername() {
 	document.getElementById("username").className = "thinking";
 
 	document.getElementById("status").src = "images/inProcess.png";
-	request = createRequest();
+	usernameRequest = createRequest();
 	
-	if (request == null) { 
+	if (usernameRequest == null) { 
 		//unable to send request
 		alert("unable to send request");
 	} else {
@@ -20,16 +20,16 @@ function checkUsername() {
 		var url = "checkName.php?username=" + username;
 		request.onreadystatechange = showUsernameStatus;
 
-		request.open("GET", url, true);
-		request.send(null);	
+		usernameRequest.open("GET", url, true);
+		usernameRequest.send(null);	
 	}
 }
 
 function showUsernameStatus() {
 	//update the page to show whether the username is okay
-	if (request.readyState == 4) {
-		if (request.status == 200) {
-			if (request.responseText == "okay") {
+	if (usernameRequest.readyState == 4) {
+		if (usernameRequest.status == 200) {
+			if (usernameRequest.responseText == "okay") {
 				// if it's okay, no error message to show
 				document.getElementById("username").className = "approved";
 				document.getElementById("register").disabled = false;
@@ -54,24 +54,24 @@ function checkPassword() {
 		return;
 	} 
 
-	var request = createRequest();
-	if (request == null) {
+	var passwordRequest = createRequest();
+	if (passwordRequest == null) {
 		//wasn't able to create request
 		return;
 	} else {
 		var password = escape(password1.value);
 		var url = "checkPass.php?password" + password;
-		request.onreadystatechange = showPasswordStatus;
-		request.open("GET", url, true);
-		request.send(null);
+		passwordRequest.onreadystatechange = showPasswordStatus;
+		passwordRequest.open("GET", url, true);
+		passwordRequest.send(null);
 	}
 }
 
 function showPasswordStatus() {
-	if (request.readyState == 4) {
-		if (request.status == 200) {
+	if (passwordRequest.readyState == 4) {
+		if (passwordRequest.status == 200) {
 			var password1 = document.getElementById("password1");
-			if (request.responseText == "okay") {
+			if (passwordRequest.responseText == "okay") {
 				password1.className = "approved";
 				document.getElementById("register").disabled = false;
 			} else {
