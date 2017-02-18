5d8b6a1d80090ba50de5c59d8edc2072d5172816
Spencer Cornelia <cornelia.spencer@yahoo.com>
2017-02-18 11:54:44 -0800

    added checkPassword and showPasswordStatus functions

--- scripts/validation.js
+++ scripts/validation.js
@@ -43,3 +43,43 @@ function showUsernameStatus() {
 		}
 	}
 }
+
+function checkPassword() {
+	var password1 = document.getElementById("password1");
+	var password2 = document.getElementById("password2");
+	password1.className = "thinking";
+
+	if ((password1.value == "") || (password1.value != password2.value)) {
+		password1.className = "denied";
+		return;
+	} 
+
+	var request = createRequest();
+	if (request == null) {
+		//wasn't able to create request
+		return;
+	} else {
+		var password = escape(password1.value);
+		var url = "checkPass.php?password" + password;
+		request.onreadystatechange = showPasswordStatus;
+		request.open("GET", url, true);
+		request.send(null);
+	}
+}
+
+function showPasswordStatus() {
+	if (request.readyState == 4) {
+		if (request.status == 200) {
+			var password1 = document.getElementById("password1");
+			if (request.responseText == "okay") {
+				password1.className = "approved";
+				document.getElementById("register").disabled = false;
+			} else {
+				password1.className = "denied";
+				password1.focus();
+				password1.select();
+				document.getElementById("register").disabled = true;
+			}
+		}
+	}
+}	
